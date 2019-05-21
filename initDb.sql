72a71bf1d65816718a386940699588ba57627f2f
Abdelkrim OUKHENNICHE <oukhennicheabdelkrim@gmail.com>
2019-05-21 18:35:59 +0200

    add users table for example

--- initDb.sql
+++ initDb.sql
@@ -5,10 +5,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `users`
---
-
 INSERT INTO `users` (`id`, `name`) VALUES
 (1, 'Jon'),(2, 'Steve');
 COMMIT;
